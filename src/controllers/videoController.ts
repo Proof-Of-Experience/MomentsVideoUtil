1e880ffdfaa03a19cfd0f1f505375821ff436479
Naim lasker <naimlasker@gmail.com>
2023-07-25 17:24:45 +0600

    video api updated

--- src/controllers/videoController.ts
+++ src/controllers/videoController.ts
@@ -10,7 +10,7 @@ export const createVideoInfo = async (req: Request, res: Response): Promise<void
     const existingVideo = await Video.findOne({ url });
 
     if (existingVideo) {
-      res.status(409).json({ error: 'Video with the same URL already exists' });
+      res.status(409).json({ status: 409, error: 'Video with the same URL already exists' });
       return;
     }
 
@@ -43,43 +43,19 @@ export const createVideoInfo = async (req: Request, res: Response): Promise<void
 
 export const getVideoInfo = async (req: Request, res: Response): Promise<void> => {
   try {
-    // const url = 'https://lvpr.tv/?v=542dr3uvnpn4ds52';
-    const url = decodeURIComponent(req.params.url);
+    const videoId = req.params.url;    
 
-    console.log('url', url);
+    // Find the video info in the database
+    const videoInfo: IVideo | null = await Video.findOne({ url: new RegExp(videoId+'$', 'i') });
 
+    // If the video info doesn't exist, send an error response
+    if (!videoInfo) {
+      res.status(404).json({ error: 'Video info not found' });
+      return;
+    }
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 720 });
-    await page.goto(url, { waitUntil: 'networkidle2' });
-
-    const videoDuration = await page.evaluate(() => {
-      const video = document.querySelector('video');
-      if (!video) {
-        throw new Error('No video found');
-      }
-      video.play();
-      return video.duration;
-    });
-
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
-    const screenshotBuffer = await page.screenshot({ fullPage: true });
-    const screenshotBase64 = `data:image/png;base64,${screenshotBuffer.toString('base64')}`;
-
-    await browser.close();
-
-    const videoInfo: IVideo = new Video({
-      url: url,
-      duration: videoDuration,
-      screenshot: screenshotBase64
-    });
-
-    await videoInfo.save();
-    console.log("Video info saved to MongoDB.");
-
-    res.json({ duration: videoDuration, screenshot: screenshotBase64 });
+    // If the video info exists, return it
+    res.json({ duration: videoInfo.duration, screenshot: videoInfo.screenshot });
   } catch (error) {
     console.error('Error processing video:', error);
     res.status(500).json({ error: 'Failed to process video' });
