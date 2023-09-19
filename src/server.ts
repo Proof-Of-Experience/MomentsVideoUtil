a4319ece1246fe09d491fe89124f1a63da8c0ffc
Naim lasker <naimlasker@gmail.com>
2023-09-20 00:24:06 +0600

    cors updated

--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,23 @@ const upload = multer();
 const app = express();
 const PORT = process.env.PORT || 3011;
 
+const corsOptions = {
+    origin: function (origin: any, callback: any) {
+        const allowedOrigins = ['http://localhost', 'http://moments.social'];
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,  // This allows cookies or authentication headers to be exchanged across origins.
+    optionsSuccessStatus: 200
+};
+
 connectDB();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use('/images', express.static(path.join(__dirname, '../', 'public', 'images')));
 app.use(express.json());
 app.use(upload.none());
