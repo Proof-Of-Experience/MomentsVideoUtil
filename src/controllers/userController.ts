669c6b47cbc7959f7404fec77dfe00f7d96b36ce
Naim lasker <naimlasker@gmail.com>
2023-10-10 03:42:32 +0600

    update user added

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -6,6 +6,11 @@ export const createUser = async (req: Request, res: Response): Promise<void> => 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST');
 
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Method Not Allowed' });
+    return;
+  }
+
   try {
     const userId = req.body.userId;
     const name = req.body.name || '';
@@ -44,19 +49,56 @@ export const createUser = async (req: Request, res: Response): Promise<void> => 
 export const getUserById = async (req: Request, res: Response): Promise<void> => {
 
   try {
-      const userId = req.params.userId; // Assuming you pass userId as a route parameter
+    const userId = req.params.userId; // Assuming you pass userId as a route parameter
 
-      const user = await User.findOne({ userId });
+    const user = await User.findOne({ userId });
 
-      if (!user) {
-          res.status(404).json({ error: 'User not found' });
-          return;
-      }
+    if (!user) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
 
-      res.status(200).json(user);
+    res.status(200).json(user);
 
   } catch (error: any) {
-      console.error('Error fetching user:', error.message);
-      res.status(500).json({ error: 'Failed to fetch user' });
+    console.error('Error fetching user:', error.message);
+    res.status(500).json({ error: 'Failed to fetch user' });
+  }
+};
+
+
+// API endpoint to patch user data
+export const updateUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = req.params.userId; // Assuming you pass userId as a route parameter
+
+    if (!userId) {
+      res.status(400).json({ error: 'User ID is not provided' });
+      return;
+    }
+
+    // Extract the fields to be updated
+    const { name, accounts } = req.body;
+
+    // Construct the update object with only provided fields
+    const updateData: Partial<IUser> = {};
+    if (name !== undefined) updateData.name = name;
+    if (accounts !== undefined) updateData.accounts = accounts;
+
+    // Update the user data
+    const updatedUser = await User.findOneAndUpdate({ userId }, updateData, {
+      new: true, // Return the updated document
+    });
+
+    if (!updatedUser) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
+
+    res.status(200).json(updatedUser);
+
+  } catch (error: any) {
+    console.error('Error updating user:', error.message);
+    res.status(500).json({ error: 'Failed to update user' });
   }
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { createPosts, getPosts } from '../controllers/postController';
 import { getTopHashtags } from '../controllers/hashController';
-import { createUser, getUserById } from '../controllers/userController';
+import { createUser, getUserById, updateUser } from '../controllers/userController';
 
 const router: Router = Router();
 
 router.get('/users/:userId', getUserById);
 router.post('/user', createUser);
+router.patch('/users/:userId', updateUser);
+
 router.get('/posts', getPosts);
 router.post('/posts', createPosts);
 router.get('/hashtags', getTopHashtags);
